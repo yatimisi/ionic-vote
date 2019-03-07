ea30b7ad19dade8c45e6e9960aab8838b2e4b96c
Yatimisi <yatimisi2012@gmail.com>
2019-03-07 23:19:30 +0800

    myBad

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -21,7 +21,7 @@ export class DataService {
       {ID:2, FirstName:'常', LastName: '洺瑋', Age: 21, Politics: '我叫常洺瑋，請投我一票～', Vote: 0},
       {ID:3, FirstName:'賴', LastName: '仟綉', Age: 20, Politics: '我叫賴仟綉，請投我一票～', Vote: 0},
       {ID:4, FirstName:'陳', LastName: '彥伊', Age: 20, Politics: '我叫陳彥伊，請投我一票～', Vote: 0},
-      {ID:4, FirstName:'黃', LastName: '鈺婷', Age: 20, Politics: '我叫黃鈺婷，請投我一票～', Vote: 0}
+      {ID:5, FirstName:'黃', LastName: '鈺婷', Age: 20, Politics: '我叫黃鈺婷，請投我一票～', Vote: 0}
     ];
   }
 
